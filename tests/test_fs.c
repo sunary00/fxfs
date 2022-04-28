594a38769f9938635be77029df4ce2db70c92066
Andrew Junzki <andrew.junzki@gmail.com>
2022-04-28 21:24:36 +0800

    add test case

--- tests/test_fs.c
+++ tests/test_fs.c
@@ -3,11 +3,47 @@
 #include "fs.h"
 
 
-START_TEST (test_find_dir) {
-    ck_assert(1 == 1);
+START_TEST ( test_init_super_block )
+{
+    initialize_superblock();
+    fxfs_context* ctx = fxfs_get_context();
+
+    ck_assert(NULL != ctx->superblock);
 } END_TEST
 
 
-int main(void) {
-    return 0;
+Suite* 
+fs_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("fxfs");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_init_super_block);
+    suite_add_tcase(s, tc_core);
+
+    // Init context
+    fxfs_init_context();
+
+    return s;
 }
+
+
+ int main(void)
+ {
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = fs_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+ }
